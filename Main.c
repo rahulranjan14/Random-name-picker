745c03473a8a2a87d3e13178cb626f9fe2d84e73
rahulranjan14 <59537577+rahulranjan14@users.noreply.github.com>
2020-03-18 12:22:12 +0530

    Create Main.c
    Using this program, names can pe picked randomly.

---
+++ Main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(0));
+
+    int i,num,random,choice;
+
+    printf("Enter the number of Players\n");
+    scanf("%d",&num);
+
+    char collection[num][15];
+
+    printf("Enter players name\n");
+
+    for(i=0;i<num;i++)
+    {
+        printf("Enter player %d Name\n",i+1);
+        scanf("%s",collection[i]);
+    }
+
+    do{
+        printf("\n");
+        random=rand()%num;
+        printf("%s\n",collection[random]);
+        printf("\n");
+        printf("Enter 1 for next turn, Enter 0 to exit\n");
+        scanf("%d",&choice);
+    }while(choice==1);
+
+    return 0;
+}
